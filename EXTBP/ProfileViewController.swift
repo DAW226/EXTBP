5bc56dfff8590055aacec5a2fb8b83c08dff78e2
Darin Wilson <DAW@Darins-MacBook-Pro.local>
2016-10-25 14:29:19 -0400

    Added everything and I don't know if that's a no no or not

--- EXTBP/ProfileViewController.swift
+++ EXTBP/ProfileViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var profileTextView: UITextView!
+    @IBOutlet weak var subHistoryTextView: UITextView!
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +30,21 @@ class ProfileViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func makeSubmission(_ sender: AnyObject) {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+    @IBAction func profSubSwtch(_ sender: UISegmentedControl) {
+        switch profileSegmentedControl.selectedSegmentIndex {
+        case 0:
+            profileTextView.isHidden = true
+            subHistoryTextView.isHidden = false
+        case 1:
+            profileTextView.isHidden = false
+            subHistoryTextView.isHidden = true
+        default:
+            break;
+        }
+    }
 
+  
 }
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'EXTBP' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
+pod 'Firebase/Storage'
+
+use_frameworks!
+
+  # Pods for EXTBP
+
+end
